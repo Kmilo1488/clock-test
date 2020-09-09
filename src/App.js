d2c040668e96fc587ba25347ebe33b74c301bb38
Kmilo1488 <camilomoreno1488@live.com>
2020-09-09 15:59:25 -0500

    Clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const useClock = () => {
+  const now = new Date()
+
+  const [ clock, setClock ] = useState(now)
+  
+  useEffect( () => {
+    updateTime()
+  });
+
+  const updateTime = () => {
+    let time = setInterval( () => timeNow(), 1000 );
+    return function update() {
+        clearInterval(time);
+    };
+  }
+  
+  const timeNow = () => {
+    setClock(new Date())
+  }
+
+  return clock
+}
+
 function App() {
+  const clock = useClock()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>{clock.toLocaleTimeString()}</span>
       </header>
     </div>
   );
